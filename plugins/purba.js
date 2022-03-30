1a9367db8b4c93153702d5ed3a3f3536ff1e68c0
Yuta Togashi <92633211+FahriAdison@users.noreply.github.com>
2022-03-30 23:06:51 +0700

    Marin-MD-V2

---
+++ plugins/purba.js
@@ -0,0 +1,10 @@
+let handler = async (m, {text}) => {
+    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+    m.reply(teks.replace(/[aiueo]/gi, '$&ve'))
+}
+handler.help = ['purba <teks>']
+handler.tags = ['fun']
+handler.command =  /^(purba)$/i
+
+
+module.exports = handler
