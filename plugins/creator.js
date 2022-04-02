e2829c2b11c94d59ea7f09f6a20da6e1b67aceb3
Hyzer <98345064+Hyzerr@users.noreply.github.com>
2022-04-03 05:43:37 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -14,7 +14,7 @@ const q = {
 	"participant": "0@s.whatsapp.net"
 }
 
-conn.sendContact(m.chat, '17608914335', 'Papah', m)
+conn.sendContact(m.chat, '6287892711054', 'Hyzer', m)
 m.reply(m.chat,'*Tuh nomor owner ku*\n*Chat jika PENTING*', q)
 }
 handler.help = ['owner', 'creator']
